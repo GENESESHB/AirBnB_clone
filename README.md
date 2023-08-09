5828ee5994c833a0423d3ba365f8ba3c9df3b163
hassan <hassan.hbmama@gmail.com>
2023-08-09 04:09:35 -0700

    add explining & some change

---
+++ README.md
@@ -0,0 +1,1 @@
+# [0x00. AirBnB clone - The console] | (https://intranet.alxswe.com/projects/263)
--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
-"""The BaseModel class"""
+
+"""defines the BaseModel class -parent-classe-"""
+
 import uuid
 from datetime import datetime
 import models
 
-
 class BaseModel:
-    '''initialize the BaseModel'''
+    '''initialization of  the BaseModel -parent-class-'''
     def __init__(self, *args, **kwargs):
-        formt = "%Y-%m-%dT%H:%M:%S.%f"
+        frmdat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, formt)
+                    value = datetime.strptime(value, frmdat)
                 if key != "__class__":
                     self.__dict__[key] = value
         else:
             models.storage.new(self)
         if not isinstance(self.created_at, datetime):
-            self.created_at = datetime.strptime(self.creadted_at, formt)
+            self.created_at = datetime.strptime(self.creadted_at, frmdat)
 
-    '''The str of the BaseModel'''
+    '''return The string of the BaseModel'''
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-    '''The update of the datetime'''
+    '''save the date of update of the datetime'''
     def save(self):
         self.updated_at = datetime.now()
         models.storage.save()
 
-    '''Return the dictionary of the BaseModel'''
+    '''
+    Return the dictionary of the BaseModel
+    send the information copy to json
+    '''
     def to_dict(self):
-        dic = self.__dict__.copy()
-        dic['__class__'] = self.__class__.__name__
-        dic['created_at'] = dic['created_at'].isoformat()
-        dic['updated_at'] = dic['updated_at'].isoformat()
-        return dic
+        r_dic = self.__dict__.copy()
+        r_dic['__class__'] = self.__class__.__name__
+        r_dic['created_at'] = r_dic['created_at'].isoformat()
+        r_dic['updated_at'] = r_dic['updated_at'].isoformat()
+        return r_dic
