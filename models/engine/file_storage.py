9969a21d2c399900e76eb0cfb4f523c7bbf5b01e
hassan <hassan.hbmama@gmail.com>
2023-08-09 15:35:30 -0700

    add some change

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-'''The FileStorage Class'''
+
+'''define the FileStorage Class
+'''
+
 from models.base_model import BaseModel
 import json
 import os
@@ -11,32 +14,38 @@ class FileStorage:
 
     classes = {"BaseModel": BaseModel}
 
-    '''Define all the dictionary objects'''
+    '''
+    Define all the dictionary objects
+    '''
     def all(self):
         return FileStorage.__objects
 
-    '''Define a new key'''
-    def new(self, obj):
-        key = obj.__class__.__name__ + '.' + obj.id
-        FileStorage.__objects[key] = obj
+    '''
+    Define a new key
+    '''
+    def new(self, objc):
+        ky = objc.__class__.__name__ + '.' + objc.id
+        FileStorage.__objects[ky] = objc
 
-    '''Define the abstracted save engine'''
+    '''
+    Define the abstracted save engine
+    '''
     def save(self):
-        my_dict = {}
-        for key, obj in FileStorage.__objects.items():
-            my_dict[key] = obj.to_dict()
+        my_dictio = {}
+        for ky, objc in FileStorage.__objects.items():
+            my_dictio[ky] = objc.to_dict()
         with open(FileStorage.__file_path, 'w', encoding="utf-8") as file:
-            json.dump(my_dict, file)
+            json.dump(my_dictio, file)
 
     '''method that deserializes the Json file and restores the objects.'''
     def reload(self):
         if not os.path.isfile(self.__file_path):
             return
         with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
-            obj = json.loads(f.read())
-            for key, value in obj.items():
-                class_name, obj_id = key.split('.')
+            objc = json.loads(f.read())
+            for ky, valu in objc.items():
+                class_name, objc_id = ky.split('.')
                 if class_name in self.classes:
                     cls_constructor = self.classes[class_name]
-                    obj_instance = cls_constructor(**value)
-                    FileStorage.__objects[key] = obj_instance
+                    objc_instance = cls_constructor(**valu)
+                    FileStorage.__objects[ky] = objc_instance
